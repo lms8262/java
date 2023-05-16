340bdf89f15f321b6d2dc677ade0927abf0d9761
lms8262 <zergling8262@gmail.com>
2023-05-16 13:13:08 +0900

    5.16 13:13

---
+++ java_study/src/ch16/sec04/LambdaExample.java
@@ -0,0 +1,17 @@
+package ch16.sec04;
+
+public class LambdaExample {
+
+	public static void main(String[] args) {
+		Person person = new Person();
+
+		person.action((x, y) -> (x + y));
+
+		person.action((x, y) -> sum(x, y));
+	}
+
+	public static double sum(double x, double y) {
+		return x + y;
+	}
+
+}
