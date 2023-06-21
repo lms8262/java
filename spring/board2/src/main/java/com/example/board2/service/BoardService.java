8874b6736bf8529784fee7909fa82d77a4ea8073
lms8262 <zergling8262@gmail.com>
2023-06-21 09:32:30 +0900

    6.21 9:32

--- spring/board2/src/main/java/com/example/board2/service/BoardService.java
+++
@@ -1,5 +0,0 @@
-package com.example.board2.service;
-
-public interface BoardService {
-
-}
--- spring/board2/src/main/java/com/example/board2/service/BoardServiceImpl.java
+++
@@ -1,8 +0,0 @@
-package com.example.board2.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class BoardServiceImpl implements BoardService {
-	
-}
